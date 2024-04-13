441a2a87c74edcdb070d5b724d8548648891c23d
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2024-04-13 14:46:41 +0530

    error corrected

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -21,7 +21,7 @@ export const connectToDatabase = async () => {
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
     cached.promise = cached.promise ||
-        mongoose.connect(MONGODB_URL, { dbName: 'pixcelpalace', bufferCommands: false })
+        mongoose.connect(MONGODB_URL, { dbName: 'saas_pixcelpalace', bufferCommands: false })
 
     cached.conn = await cached.promise;
 
