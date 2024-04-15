4555be93e1974e1b3e74ed5f33265f22ab2ced7c
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2024-04-16 00:44:22 +0530

    Image Transformation and Saving data in database

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -27,6 +27,10 @@ import { useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { updateCredits } from "@/lib/actions/user.actions"
 import MediaUploader from "./MediaUploader"
+import TransformedImage from "./TransformedImage"
+import { getCldImageUrl } from "next-cloudinary"
+import { addImage, updateImage } from "@/lib/actions/image.actions"
+import { useRouter } from "next/navigation"
 
 export const formSchema = z.object({
     title: z.string(),
@@ -47,6 +51,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
     const [isTransforming, setIsTransforming] = useState(false);
     const [transformationConfig, setTransformationConfig] = useState(config);
     const [isPending, startTransition] = useTransition();
+    const router = useRouter();
 
     const initialValues = data && action === 'Update' ? {
         title: data?.title,
@@ -62,8 +67,70 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
     })
 
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        setIsSubmitting(true)
+
+        if (data || image) {
+            const transformationUrl = getCldImageUrl({
+                width: image.width,
+                height: image?.height,
+                src: image?.publicId,
+                ...transformationConfig
+            })
+
+            const imageData = {
+                title: values.title,
+                publicId: image?.publicId,
+                transformationType: type,
+                width: image?.width,
+                height: image?.height,
+                config: transformationConfig,
+                secureURL: image?.secureURL,
+                transformationURL: transformationUrl,
+                aspectRatio: values.aspectRatio,
+                prompt: values.prompt,
+                color: values.color
+            }
+
+            if (action === 'Add') {
+                try {
+                    const newImage = await addImage({
+                        image: imageData,
+                        userId,
+                        path: '/'
+                    })
+
+                    if (newImage) {
+                        form.reset()
+                        setImage(data);
+                        router.push(`/transformations/${newImage._id}`)
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            if (action === 'Update') {
+                try {
+                    const updatedImage = await updateImage({
+                        image: {
+                            ...imageData,
+                            _id: data._id
+                        },
+                        userId,
+                        path: `/transformations/${data._id}`
+                    })
+
+                    if (updatedImage) {
+                        router.push(`/transformations/${updatedImage._id}`)
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+
+        setIsSubmitting(false);
     }
 
     const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
@@ -205,11 +272,19 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                             />
                         )}
                     />
+                    <TransformedImage
+                        image={image}
+                        type={type}
+                        title={form.getValues().title}
+                        isTransforming={isTransforming}
+                        setIsTransforming={setIsTransforming}
+                        transformationConfig={transformationConfig}
+                    />
                 </div>
 
                 <div className="flex flex-col gap-4">
                     <Button type="button"
-                        className="submit-button capitalize"
+                        className="submit-button capitalize cursor-pointer"
                         disabled={isTransforming || newTransformation === null}
                         onClick={onTransformHandler}
                     >
